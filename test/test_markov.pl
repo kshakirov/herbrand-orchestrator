0acf3d5ec334b99d58819ae0e913e4aca556c617
Kirill Shakirov <kirill.shakirov4@gmail.com>
2026-09-14 17:22:30 +0300

    wip 2

--- test/test_markov.pl
+++ test/test_markov.pl
@@ -1,7 +1,12 @@
-markov_rule(abc, xyz).
-markov_rule(xxx, aaa).
-markov_rule(aaa, abc).
+:- use_module(library(portray_text)).
+:-set_portray_text(enabled, true).
 
+
+:- set_prolog_flag(double_quotes, codes).
+
+markov_rule("abc", "xyz").
+markov_rule("xxx", "aaa").
+markov_rule("aaa","abc").
 
 markov_step(String, Next):-
     (   markov_rule(String, Replacement),
@@ -14,11 +19,11 @@ markov_step(String, Next):-
     write("NExt "),
     write(Next).
 
-test_cut_prefix(L, Len1,0,R):- test_cut_prefix(L,Len1,0,R, []);   write("\nfail"), R = [].
+test_cut_prefix(L, Len1,0,R):- test_cut_prefix(L,Len1,0,R, []);  format("Fail test_cut_prefix ~n"), R = [].
 test_cut_prefix([], Len,Len,R,R).
 test_cut_prefix([], Len1,Len2,R,X):-
     Len1 \== Len2,
-    write("fdfdfd"),
+
     fail.
    
 
@@ -30,6 +35,21 @@ test_cut_prefix([H|T], Len1,Len2, Acc, Tail):-
     test_cut_prefix(T, Len1 , Len3, DiffTail,Tail).
     
 
-    
+
+markov_step_mod(String, Next):-
+    (   markov_rule(Template, R),
+	length(Template,N),
+	test_cut_prefix(String,N,0,Fragment),
+	format( "Fragment before rule ~s~n", [Fragment]),
+	markov_rule(Fragment, Replacement),
+	format("Fragment after rule ~s~n", [Replacement]),
+	format("Finishing"),
+	!,
+	
+    markov_step_mod(Replacement,Next));
+    Next =String,
+   format("Next~n "),
+   format("~s~n", [Next]).
+
     
     
